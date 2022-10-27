9490cdaafb51f495eccf52c3e31f098c399267ed
xmslyz <xmslyz.gmail.com>
2022-10-27 15:59:11 +0200

    po poprawkach z pierwszego PR

--- Income/stipendIncome.py
+++ Income/stipendIncome.py
@@ -1,26 +1,24 @@
 from Income.income import Income
 
 
-class massStipend(Income):
+class MassStipend(Income):
 
     def __init__(self, income_type, amount, who_recived, who_applied):
         self.type = income_type
         self.amount = amount
-        self.recived = who_recived
-        self.applied = who_applied
+        self.priest_reciving = who_recived
+        self.priest_applying = who_applied
 
     @property
     def amount(self):
         return self.__amount
     @amount.setter
     def amount(self, amount):
-        if amount < 0:
-            self.__amount = 0
-        else:
-            self.__amount = amount
+        # self.__amount = 0 if amount < 0 else self.__amount = amount
+        self.__amount = amount
 
     def show_stipend(self):
-        print(f"{self.type} w wysokości {self.amount} przyjął {self.recived}. Mszę odprawił {self.applied}")
+        print(f"{self.type} w wysokości {self.amount} przyjął {self.priest_reciving}. Mszę odprawił {self.priest_applying}")
 
     def show_type(self):
         print(self.type)
@@ -29,30 +27,23 @@ class massStipend(Income):
         return self.__amount
 
 
-class count_stipends(massStipend):
-
-    # zwraca sumę przyjętych ofiar za msze przez konkretnego ks.
-    def sum_stipends_for_reciver(sack, receiver):
-        suma = 0
-        for stipends in sack:
-            if stipends.recived == receiver:
-                suma += stipends.amount
-        return suma
+class CountStipends(MassStipend):
 
     # zwraca listę przyjętych ofiar za msze przez konkretnego ks.
-    def list_of_stipends_for_reciever(sack, receiver):
+    def list_of_stipends_recieved_by_a_priest(sack, receiving_money):
         lista = []
-        for stipends in sack:
-            if stipends.recived == receiver:
-                lista.append(stipends.amount)
+        for stipend in sack:
+            if stipend.priest_reciving == receiving_money:
+                lista.append(stipend.amount)
         return lista
+
+    # zwraca sumę przyjętych ofiar za msze przez konkretnego ks.
+    def sum_stipends_recived_by_a_priest(sack, receiving_money):
+        return sum(CountStipends.list_of_stipends_recieved_by_a_priest(sack, receiving_money))
 
     # zwraca sumę wszystkich przyjętych ofiar za msze
     def sum_all_stipends(sack):
-        suma = 0
-        for stipends in sack:
-            suma += stipends.amount
-        return suma
+        return sum([stipend.amount for stipend in sack])
 
     # zwraca ilość przyjętych mszy
     def sum_of_masses(sack):
@@ -60,16 +51,12 @@ class count_stipends(massStipend):
 
     # zwraca średnią (stypendium)
     def mediana_stipends(sack):
-        return count_stipends.sum_all_stipends(sack) / count_stipends.sum_of_masses(sack)
+        return CountStipends.sum_all_stipends(sack) / CountStipends.sum_of_masses(sack)
 
     # zwraca ilość odprawionych mszy przez danego ks.
     def sum_of_applied_masses(sack, applier):
-        suma = 0
-        for stipends in sack:
-            if stipends.applied == applier:
-                suma += 1
-        return suma
+        return len([stipend for stipend in sack if stipend.priest_applying == applier])
 
     # zwraca quotę za odprawione msze
     def quota(sack, applier):
-        return count_stipends.mediana_stipends(sack) * count_stipends.sum_of_applied_masses(sack, applier)
+        return CountStipends.mediana_stipends(sack) * CountStipends.sum_of_applied_masses(sack, applier)
--- Random_Generator/random_values_generator.py
+++ Random_Generator/random_values_generator.py
@@ -1,28 +1,28 @@
 import random
-from Income.stipendIncome import massStipend, count_stipends
+from Income.stipendIncome import MassStipend, CountStipends
 
 
 def random_values_generator():
     n = random.randint(150, 220)  # zakres przyjętych w miesiącu mszy
     i = 0
     sack = []
-    amounts = [-12, 50, 60, 70, 80]  # przykładowe ofiary za mszę
+    amounts = [100, 50, 60, 70, 80]  # przykładowe ofiary za mszę
     priests = ["p1", "p2", "p3", "p4", "p5"]  # lista księży na parafii
 
     # generator na potrzeby testu funkcji
-    while i < n:
-        j = massStipend("Stypendium", random.choice(amounts), random.choice(priests), random.choice(priests))
+    for _ in range(n):
+        j = MassStipend("Stypendium", random.choice(amounts), random.choice(priests), random.choice(priests))
         sack.append(j)
         i += 1
 
     # do konsoli
-    print(f'Suma wszystkich stypendiów w tym miesiącu wynosi: {count_stipends.sum_all_stipends(sack)} zł')
-    print(f'Ilość mszy w miesiącu wyniosła: {count_stipends.sum_of_masses(sack)}')
-    print(f'{count_stipends.mediana_stipends(sack):.2f} zł')
+    print(f'Suma wszystkich stypendiów w tym miesiącu wynosi: {CountStipends.sum_all_stipends(sack)} zł')
+    print(f'Ilość mszy w miesiącu wyniosła: {CountStipends.sum_of_masses(sack)}')
+    print(f'{CountStipends.mediana_stipends(sack):.2f} zł')
     print("")
     for x in priests:
-        print(f'{x} przyjął stypendiów w kwocie {count_stipends.sum_stipends_for_reciver(sack, x)} zł')
-        print(f'Lista przyjętych stypendiów {count_stipends.list_of_stipends_for_reciever(sack, x)}')
-        print(f'{x} odprawił {count_stipends.sum_of_applied_masses(sack, x)} mszy.')
-        print(f'{count_stipends.quota(sack, x):.2f} zł')
+        print(f'{x} przyjął stypendiów w kwocie {CountStipends.sum_stipends_recived_by_a_priest(sack, x)} zł')
+        print(f'Lista przyjętych stypendiów {CountStipends.list_of_stipends_recieved_by_a_priest(sack, x)}')
+        print(f'{x} odprawił {CountStipends.sum_of_applied_masses(sack, x)} mszy.')
+        print(f'{CountStipends.quota(sack, x):.2f} zł')
         print()
