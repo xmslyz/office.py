0bb11066283e6ba417d3043eab146d88a7667252
xmslyz <xmslyz.gmail.com>
2022-11-10 11:54:41 +0100

    TestDBSettings [passed]

--- buissnes/Database/AtributesSetter.py
+++ buissnes/Database/AtributesSetter.py
@@ -22,7 +22,6 @@ class DBSettings:
         new_dbname = re.sub(r'[^A-Za-z0-9_]+', '', os.path.splitext(str(value).strip())[0])
         self.__dbname = f'{new_dbname}.db'
 
-
     @property
     def db_path(self):
         return self.__path
@@ -30,15 +29,9 @@ class DBSettings:
     def db_path(self):
         return self.__path
     @db_path.setter
-    # def db_path(self, *args):
-    #     new_path = ''
-    #     for arg in args:
-    #         print(arg)
-    #         sub_path = re.sub(r'[^A-Za-z0-9\\_]+', '', os.path.splitext(str(arg).strip())[0])
-    #         new_path += sub_path + '\\'
-    #     self.path = new_path
     def db_path(self, value):
         new_path = re.sub(r'[^A-Za-z0-9\\_]+', '', str(value).strip())
+        new_path = new_path.rstrip('\\')
         self.__path = new_path
 
     @property
@@ -49,6 +42,8 @@ class DBSettings:
         return self.__table_name
     @db_table_name.setter
     def db_table_name(self, value):
+        if value == "":
+            raise Exception("Nazwa tabeli nie może być pusta.")
         new_table_name = re.sub(r'[^A-Za-z0-9_]+', '', os.path.splitext(str(value).strip())[0])
         self.__table_name = new_table_name
 
@@ -59,9 +54,8 @@ class DBSettings:
     def db_full_path(self):
         return self.__full_path
     @db_full_path.setter
-    def db_full_path(self, x):
-        x += x
-        self.__full_path = os.path.join(os.path.abspath(os.getcwd()), self.__path, self.__dbname)
+    def db_full_path(self, x=1):
+        self.__full_path = os.path.join(os.path.abspath(os.getcwd()), f'{self.__path}\\', self.__dbname)
 
 
 class TableSettings(DBSettings):
--- tests/test_DBSettings.py
+++ tests/test_DBSettings.py
@@ -18,33 +18,49 @@ class TestDBName(TestCase):
 
     def test_dbname__name_with_ext(self):
         test_db = DBS.DBSettings()
-        test_db.db_name = "database.com.pl"
+        test_db.db_name = "database.pl"
         print(test_db.db_name)
         assert test_db.db_name == "database.db"
 
 
+class TestDBPath(TestCase):
 
-class TestDBSettings(TestCase):
-
-    def test_dbname(self):
+    def test_path__nonalfanumeric(self):
         test_db = DBS.DBSettings()
-        test_db.db_name = "da*ta+ba/se_na(me_test"
-        assert test_db.db_name == "database_name_test.db"
+        test_db.db_path = "Q$QQQ\\a+-aa\\"
+        print(test_db.db_path)
+        assert test_db.db_path == "QQQQ\\aaa"
 
-    def test_path(self):
+    def test_path__with_slashes(self):
         test_db = DBS.DBSettings()
-        test_db.db_path = "QQQQ\\aaa\\"
-        assert test_db.db_path == "QQQQ\\aaa\\"
+        test_db.db_path = "Q$QQQ\\a+-aa\\\\"
+        print(test_db.db_path)
+        assert test_db.db_path == "QQQQ\\aaa"
 
-    def test_table_name(self):
+
+class TestDBTablename(TestCase):
+    def test_dbtablename(self):
         test_db = DBS.DBSettings()
         test_db.db_table_name = "da@ta#b$a$s$e_t{a{b  l e      "
         assert test_db.db_table_name == "database_table"
 
-    def test_full_path(self):
+    def test_dbtablename__empty(self):
         test_db = DBS.DBSettings()
-        cwd = os.getcwd()
-        test_db.db_name = "da*ta+ba/se_na(me_test"
-        test_db.db_path = "QQQQ\\aaa\\"
-        test_db.db_full_path = 1
-        assert test_db.db_full_path == f"{cwd}\\QQQQ\\aaa\\database_name_test.db"
+        with self.assertRaises(Exception) as context:
+            test_db.db_table_name = ''
+        self.assertTrue('Nazwa tabeli nie może być pusta.' in str(context.exception))
+
+    def test_dbtablename__name_with_ext(self):
+        test_db = DBS.DBSettings()
+        test_db.db_table_name = "database.pl"
+        print(test_db.db_table_name)
+        assert test_db.db_table_name == "database"
+
+
+class TestDBFullPath(TestCase):
+    def test_fullpath(self):
+        test_db = DBS.DBSettings()
+        test_db.db_name = "test.db"
+        test_db.db_path = "TEST\\test\\"
+        test_db.db_full_path = ''
+        assert test_db.db_full_path == f'{os.getcwd()}\\TEST\\test\\test.db'
