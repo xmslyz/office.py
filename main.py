381dad48e0d7f008655bc65418c11b9dcfe42073
xmslyz <xmslyz.gmail.com>
2022-11-19 22:36:13 +0100

    Zmiany raz jeszcze. Cała sobota jak krew w piach.

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import buisness.Input.Caster
-import gui.InsertTab.UpdateMassRecord
-import gui.InsertTab.DeleteMassRecord
-import gui.InsertTab.InsertNewMassRecord
-import gui.SettingsTab.Button_BuildNewDatabase
-import gui.InsertTab.UpdateStmts
 
 
 def main():
-    pass
-    # gui.SettingsTab.Button_BuildNewDatabase.Build_DB_Button().build_database()
-    # gui.InsertTab.InsertNewMassRecord.Button_Add_New_Record().add_new_mass_record()
-    # gui.InsertTab.UpdateMassRecord.Button_Update_Mass_Record().update_intention_row()
-    # gui.InsertTab.DeleteMassRecord.Button_Delete_Mass_Record().delete_intention_row()
-    # gui.InsertTab.DeleteMassRecord.Button_Delete_Mass_Record().delete_last()
-    # gui.InsertTab.UpdateStmts.ButtonUpdateMonthlyStmt().update_monthly_stmt('2022-10')
-    # gui.InsertTab.UpdateStmts.ButtonUpdateGeneralStmt().update_general_stmt('2022-10')
-    ####  gui.SettingsTab.Button_BuildNewDatabase.Build_DB_Button().build_database()
-    ####  gui.SettingsTab.Button_BuildNewDatabase.Drop_DB_Button().drop_database()
-    ####  gui.SettingsTab.Button_BuildNewDatabase.Remove_DB_Files.remove_db_file()
     print("hi")
 
 
